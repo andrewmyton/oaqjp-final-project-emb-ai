15efeb0fb2484360978e81cba3ea71af3ac36021
Andrew <Andrew.Myton@icloud.com>
2025-04-28 14:44:34 -0400

    added test file

---
+++ test_emotion_detection.py
@@ -0,0 +1,10 @@
+from EmotionDetection.emotion_detection import emotion_detector
+statements = 
+["I am glad this happened",
+ "I am really mad about this",
+ "I feel disgusted just hearing about this",
+ "I am so sad about this",
+ "I am really afraid that this will happen"]
+
+ for sentence in statements:
+    emotion_detector(sentence)
